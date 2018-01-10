768f52052c2a5419dd654cc4ec5997805aca4e7a
josephscottstevens <josephscottstevens@gmail.com>
2018-01-10 16:57:55 -0500

    t

--- src/Billing.elm
+++ src/Billing.elm
@@ -37,8 +37,7 @@ type alias Model =
     { rows : List Row
     , tableState : Table.State
     , query : String
-    , currentPage : Int
-    , count : Int
+    , gridOperations : GridOperations
     }
 
 
@@ -91,20 +90,14 @@ type alias Row =
 
 view : Model -> Maybe AddEditDataSource -> Html Msg
 view model addEditDataSource =
-    let
-        rows =
-            filteredCcm model
-                |> List.drop (model.currentPage * pagesPerBlock)
-                |> List.take itemsPerPage
-    in
-        div []
-            [ button [ class "btn btn-default", onClick Reset ] [ text "reset" ]
-            , input [ class "form-control", placeholder "Search by Facility" ] []
-            , div [ class "e-grid e-js e-waitingpopup" ]
-                [ Table.view (config addEditDataSource model.tableState) model.tableState rows
-                ]
-            , pagingView model.currentPage model.count
+    div []
+        [ button [ class "btn btn-default", onClick Reset ] [ text "reset" ]
+        , input [ class "form-control", placeholder "Search by Facility" ] []
+        , div [ class "e-grid e-js e-waitingpopup" ]
+            [ Table.view (config addEditDataSource model.tableState) model.tableState model.rows
             ]
+        , pagingView model.gridOperations
+        ]
 
 
 type Msg
@@ -119,7 +112,7 @@ update : Msg -> Model -> Int -> ( Model, Cmd Msg )
 update msg model patientId =
     case msg of
         Load (Ok t) ->
-            { model | rows = t.result, count = t.count } ! []
+            { model | rows = t.result } ! []
 
         Load (Err t) ->
             model ! [ Functions.displayErrorMessage (toString t) ]
@@ -127,7 +120,7 @@ update msg model patientId =
         SetPagingState page ->
             let
                 newPageIndex =
-                    getNewState page model.currentPage model.count
+                    getNewState page model.gridOperations
             in
                 { model | currentPage = newPageIndex } ! []
 
@@ -155,21 +148,14 @@ filteredCcm model =
 -- Paging stuff
 
 
-itemsPerPage : Int
-itemsPerPage =
-    10
-
-
-pagesPerBlock : Int
-pagesPerBlock =
-    8
-
-
-getNewState : Page -> Int -> Int -> Int
-getNewState page currentPage totalRows =
+getNewState : Page -> GridOperations -> Int
+getNewState page gridOperations =
     let
         totalPages =
-            totalRows // itemsPerPage
+            gridOperations.totalRows // gridOperations.rowsPerPage
+
+        currentPage =
+            gridOperations.skip // gridOperations.rowsPerPage
     in
         case page of
             First ->
@@ -195,95 +181,6 @@ getNewState page currentPage totalRows =
 
             Last ->
                 totalPages - 1
-
-
-pagingView : Int -> Int -> Html Msg
-pagingView currentPage totalVisiblePages =
-    let
-        totalPages =
-            (totalVisiblePages // itemsPerPage) - 1
-
-        activeOrNot pageIndex =
-            let
-                activeOrNotText =
-                    if pageIndex == currentPage then
-                        "e-currentitem e-active"
-                    else
-                        "e-default"
-            in
-                div
-                    [ class ("e-link e-numericitem e-spacing " ++ activeOrNotText), onClick (SetPagingState (Index pageIndex)) ]
-                    [ text (toString (pageIndex + 1)) ]
-
-        rng =
-            List.range 0 totalPages
-                |> List.drop ((currentPage // pagesPerBlock) * pagesPerBlock)
-                |> List.take pagesPerBlock
-                |> List.map activeOrNot
-
-        firstPageClass =
-            if currentPage >= pagesPerBlock then
-                "e-icon e-mediaback e-firstpage e-default"
-            else
-                "e-icon e-mediaback e-firstpagedisabled e-disable"
-
-        leftPageClass =
-            if currentPage > 0 then
-                "e-icon e-arrowheadleft-2x e-prevpage e-default"
-            else
-                "e-icon e-arrowheadleft-2x e-prevpagedisabled e-disable"
-
-        leftPageBlockClass =
-            if currentPage >= pagesPerBlock then
-                "e-link e-spacing e-PP e-numericitem e-default"
-            else
-                "e-link e-nextprevitemdisabled e-disable e-spacing e-PP"
-
-        rightPageBlockClass =
-            if currentPage < totalPages - pagesPerBlock then
-                "e-link e-NP e-spacing e-numericitem e-default"
-            else
-                "e-link e-NP e-spacing e-nextprevitemdisabled e-disable"
-
-        rightPageClass =
-            if currentPage < totalPages then
-                "e-nextpage e-icon e-arrowheadright-2x e-default"
-            else
-                "e-icon e-arrowheadright-2x e-nextpagedisabled e-disable"
-
-        lastPageClass =
-            if currentPage < totalPages - pagesPerBlock then
-                "e-lastpage e-icon e-mediaforward e-default"
-            else
-                "e-icon e-mediaforward e-animate e-lastpagedisabled e-disable"
-
-        pagerText =
-            let
-                currentPageText =
-                    toString (currentPage + 1)
-
-                totalPagesText =
-                    toString (totalPages + 1)
-
-                totalItemsText =
-                    toString totalVisiblePages
-            in
-                currentPageText ++ " of " ++ totalPagesText ++ " pages (" ++ totalItemsText ++ " items)"
-    in
-        div [ class "e-pager e-js e-pager" ]
-            [ div [ class "e-pagercontainer" ]
-                [ div [ class firstPageClass, onClick (SetPagingState First) ] []
-                , div [ class leftPageClass, onClick (SetPagingState Previous) ] []
-                , a [ class leftPageBlockClass, onClick (SetPagingState PreviousBlock) ] [ text "..." ]
-                , div [ class "e-numericcontainer e-default" ] rng
-                , a [ class rightPageBlockClass, onClick (SetPagingState NextBlock) ] [ text "..." ]
-                , div [ class rightPageClass, onClick (SetPagingState Next) ] []
-                , div [ class lastPageClass, onClick (SetPagingState Last) ] []
-                ]
-            , div [ class "e-parentmsgbar", style [ ( "text-align", "right" ) ] ]
-                [ span [ class "e-pagermsg" ] [ text pagerText ]
-                ]
-            ]
 
 
 decodeBillingCcm : Decode.Decoder Row
@@ -328,36 +225,145 @@ decodeBillingCcm =
         |> Pipeline.required "Is24HoursSinceBilled" (Decode.bool)
 
 
+pagingView : GridOperations -> Html Msg
+pagingView gridOperations =
+    let
+        totalPages =
+            (gridOperations.totalVisiblePages // gridOperations.itemsPerPage) - 1
+
+        currentPage =
+            gridOperations.skip // gridOperations.rowsPerPage
+
+        activeOrNot pageIndex =
+            let
+                activeOrNotText =
+                    if pageIndex == currentPage then
+                        "e-currentitem e-active"
+                    else
+                        "e-default"
+            in
+                div
+                    [ class ("e-link e-numericitem e-spacing " ++ activeOrNotText), onClick (SetPagingState (Index pageIndex)) ]
+                    [ text (toString (pageIndex + 1)) ]
+
+        rng =
+            List.range 0 totalPages
+                |> List.drop ((currentPage // gridOperations.pagesPerBlock) * gridOperations.pagesPerBlock)
+                |> List.take gridOperations.pagesPerBlock
+                |> List.map activeOrNot
+
+        firstPageClass =
+            if currentPage >= gridOperations.rowsPerPage then
+                "e-icon e-mediaback e-firstpage e-default"
+            else
+                "e-icon e-mediaback e-firstpagedisabled e-disable"
+
+        leftPageClass =
+            if currentPage > 0 then
+                "e-icon e-arrowheadleft-2x e-prevpage e-default"
+            else
+                "e-icon e-arrowheadleft-2x e-prevpagedisabled e-disable"
+
+        leftPageBlockClass =
+            if currentPage >= gridOperations.pagesPerBlock then
+                "e-link e-spacing e-PP e-numericitem e-default"
+            else
+                "e-link e-nextprevitemdisabled e-disable e-spacing e-PP"
+
+        rightPageBlockClass =
+            if currentPage < totalPages - gridOperations.pagesPerBlock then
+                "e-link e-NP e-spacing e-numericitem e-default"
+            else
+                "e-link e-NP e-spacing e-nextprevitemdisabled e-disable"
+
+        rightPageClass =
+            if currentPage < totalPages then
+                "e-nextpage e-icon e-arrowheadright-2x e-default"
+            else
+                "e-icon e-arrowheadright-2x e-nextpagedisabled e-disable"
+
+        lastPageClass =
+            if currentPage < totalPages - gridOperations.pagesPerBlock then
+                "e-lastpage e-icon e-mediaforward e-default"
+            else
+                "e-icon e-mediaforward e-animate e-lastpagedisabled e-disable"
+
+        pagerText =
+            let
+                currentPageText =
+                    toString (currentPage + 1)
+
+                totalPagesText =
+                    toString (totalPages + 1)
+
+                totalItemsText =
+                    toString gridOperations.totalVisiblePages
+            in
+                currentPageText ++ " of " ++ totalPagesText ++ " pages (" ++ totalItemsText ++ " items)"
+    in
+        div [ class "e-pager e-js e-pager" ]
+            [ div [ class "e-pagercontainer" ]
+                [ div [ class firstPageClass, onClick (SetPagingState First) ] []
+                , div [ class leftPageClass, onClick (SetPagingState Previous) ] []
+                , a [ class leftPageBlockClass, onClick (SetPagingState PreviousBlock) ] [ text "..." ]
+                , div [ class "e-numericcontainer e-default" ] rng
+                , a [ class rightPageBlockClass, onClick (SetPagingState NextBlock) ] [ text "..." ]
+                , div [ class rightPageClass, onClick (SetPagingState Next) ] []
+                , div [ class lastPageClass, onClick (SetPagingState Last) ] []
+                ]
+            , div [ class "e-parentmsgbar", style [ ( "text-align", "right" ) ] ]
+                [ span [ class "e-pagermsg" ] [ text pagerText ]
+                ]
+            ]
+
+
 type alias GridOperations =
     { skip : Int
     , pageSize : Int
+    , rowsPerPage : Int
+    , totalRows : Int
     , sortField : Maybe String
     , sortAscending : Maybe Bool
     }
 
 
-testGridOperations : GridOperations
-testGridOperations =
+sampleGridOperations : GridOperations
+sampleGridOperations =
     { skip = 0
     , pageSize = 10
+    , rowsPerPage = 20
+    , totalRows = -1
     , sortField = Just "DoB"
     , sortAscending = Just False
     }
 
 
-testBody : GridOperations -> Encode.Value
-testBody gridOperations =
+encodeResponse : GridOperations -> Encode.Value
+encodeResponse gridOperations =
     Encode.object
         [ ( "Skip", Encode.int gridOperations.skip )
         , ( "PageSize", Encode.int gridOperations.pageSize )
+        , ( "RowsPerPage", Encode.int gridOperations.rowsPerPage )
+        , ( "TotalRows", Encode.int gridOperations.totalRows )
         , ( "SortField", maybeVal Encode.string gridOperations.sortField )
         , ( "SortAscending", maybeVal Encode.bool gridOperations.sortAscending )
         ]
 
 
+decodeResponse : Decode.Decoder GridOperations
+decodeResponse =
+    Pipeline.decode GridOperations
+        |> Pipeline.required "Skip" Decode.int
+        |> Pipeline.required "PageSize" Decode.int
+        |> Pipeline.required "RowsPerPage" Decode.int
+        |> Pipeline.required "TotalRows" Decode.int
+        |> Pipeline.required "SortField" (Decode.maybe Decode.string)
+        |> Pipeline.required "SortAscending" (Decode.maybe Decode.bool)
+
+
 type alias LoadResult =
     { result : List Row
-    , count : Int
+    , gridOperations : GridOperations
     }
 
 
@@ -365,13 +371,13 @@ jsonDecodeLoad : Decode.Decoder LoadResult
 jsonDecodeLoad =
     Pipeline.decode LoadResult
         |> Pipeline.required "result" (Decode.list decodeBillingCcm)
-        |> Pipeline.required "count" Decode.int
+        |> Pipeline.required "gridOperations" decodeResponse
 
 
 load : Int -> Cmd Msg
 load patientId =
     jsonDecodeLoad
-        |> Functions.postJsonRequest (testBody testGridOperations) ("/People/BillingTest?patientId=" ++ toString patientId)
+        |> Functions.postJsonRequest (encodeResponse sampleGridOperations) ("/People/BillingTest?patientId=" ++ toString patientId)
         |> Http.send Load
 
 
@@ -381,7 +387,7 @@ emptyModel =
     , tableState = Table.initialSort "Date"
     , query = ""
     , currentPage = 0
-    , count = 10
+    , gridOperations = sampleGridOperations
     }
 
 
